7126551878ef1adcdd10fc7b774d850581e5ef75
mimiramita <rdhamrongsir@umass.edu>
2023-05-11 17:15:24 -0400

    build frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="home" element={<Home />} />
+          <Route path="login" element={<Login />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Login() {
+    return (
+        <h1>this is the login page</h1>
+    );
+}
+
+export default Login;
