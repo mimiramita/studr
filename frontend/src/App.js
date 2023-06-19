7ceaef206f6707815087f9045f66bcd7ae494e4f
mimiramita <rdhamrongsir@umass.edu>
2023-06-19 16:30:12 -0400

    create query box prototype

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import CreateProject from "./pages/CreateProject";
+import QueryBox from "./pages/Query";
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
           <Route path="home" element={<Home />} />
           <Route path="login" element={<Login />} />
           <Route path="createproject" element={<CreateProject />} />
+          <Route path="query" element={<QueryBox />} />
         </Route>
       </Routes>
     </BrowserRouter>
